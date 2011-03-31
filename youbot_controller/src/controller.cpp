40f542926aef50d08c5257c33d8199313f7babfe
Steven Bellens <steven.bellens@mech.kuleuven.be>
2011-03-31 21:12:39 +0200

    youbot_controller: adding documentation
    Signed-off-by: Steven Bellens <steven.bellens@mech.kuleuven.be>

--- youbot_controller/src/controller.cpp
+++ youbot_controller/src/controller.cpp
@@ -34,20 +34,22 @@
 *       POSSIBILITY OF SUCH DAMAGE.                                           *
 *                                                                             *
 *******************************************************************************/
-
 #include "controller.hpp"
 
+// This macro is used whenever a new Orocos component is generated. Here we
+// generate a Youbot Controller component.
 ORO_CREATE_COMPONENT(youbot::Controller)
 
 namespace youbot{
   Controller::Controller(std::string name) : TaskContext(name,PreOperational)
   ,m_goal_tolerance(3,0.0)
   ,m_velocity(3,0.0)
-  ,m_time_passed(0.0)
   {
+    /// Add the input and output ports to the Orocos interface
     this->addPort("current_pose",current_pose_port).doc("Youbot pose");
     this->addPort("goal_pose",goal_pose_port).doc("Youbot goal pose");
     this->addPort("ctrl",ctrl_port).doc("Youbot control input");
+    /// Add property variables to the Orocos interface
     this->addProperty("goal_tolerance",m_goal_tolerance).doc("Tolerance on goal pose [x y yaw]");
     this->addProperty("control_velocity",m_velocity).doc("Control velocity");
   }
@@ -55,27 +57,22 @@ namespace youbot{
   Controller::~Controller(){}
 
   bool Controller::configureHook(){
-    m_delta_pose.resize(3);
     return true;
   }
 
   bool Controller::startHook(){
-    m_time_begin = RTT::os::TimeService::Instance()->getTicks();
     return true;
   }
 
   void Controller::updateHook(){
-    /// get the time since the last update
-    m_time_passed = RTT::os::TimeService::Instance()->secondsSince(m_time_begin);
-    /// read in current pose and goal pose
-    goal_pose_port_status = goal_pose_port.read(m_goal_pose);
-    current_pose_port_status = current_pose_port.read(m_current_pose);
-
-    if(goal_pose_port_status != NoData && current_pose_port_status != NoData){
+    /// read in current pose and goal pose - notice that whenever one of the
+    //ports does not have any data on it, the controller will tell the YouBot
+    //not to move.
+    if(goal_pose_port.read(m_goal_pose) != NoData && current_pose_port.read(m_current_pose) != NoData){
       /// calculate the pose difference
       calcPoseDiff();
-      // generate control inputs
-      // don't do anything for those DOFs that are within the specified tolerance
+      // Generate control inputs, but don't do anything for those DOFs that are within the specified tolerance
+      // X
       if(abs(m_delta_pose[0]) > m_goal_tolerance[0]){
         if(m_delta_pose[0] > 0) m_ctrl.linear.x = abs(m_velocity[0]);
         else m_ctrl.linear.x = -abs(m_velocity[0]);
@@ -83,6 +80,7 @@ namespace youbot{
       else{
         m_ctrl.linear.x = 0.0;
       }
+      // Y
       if(abs(m_delta_pose[1]) > m_goal_tolerance[1]){
         if(m_delta_pose[1] > 0) m_ctrl.linear.y = abs(m_velocity[1]);
         else m_ctrl.linear.y = -abs(m_velocity[1]);
@@ -90,6 +88,7 @@ namespace youbot{
       else{
         m_ctrl.linear.y = 0.0;
       }
+      // Theta
       if(abs(m_delta_pose[2]) > m_goal_tolerance[2]){
         if(m_delta_pose[2] > 0) m_ctrl.angular.z = abs(m_velocity[2]);
         else m_ctrl.angular.z = -abs(m_velocity[2]);
@@ -98,35 +97,28 @@ namespace youbot{
         m_ctrl.angular.z = 0.0;
       }
     }
+    // One of the input ports did not contain any data, find out which one and
+    // signal this to the user
     else{
-      if(goal_pose_port_status == NoData){
-        log(Debug) << "No goal pose received" << endlog();
-      }
-      if(current_pose_port_status == NoData){
-        log(Debug) << "No pose estimate received" << endlog();
-      }
+      log(Debug) << "No goal pose and/or current pose received" << endlog();
+      // Tell the YouBot not to move
       m_ctrl.linear.x = 0.0;
       m_ctrl.linear.y = 0.0;
-      m_ctrl.linear.z = 0.0;
       m_ctrl.angular.z = 0.0;
     }
-    // write out
+    // Write the control values to the ctrl output port
     ctrl_port.write(m_ctrl);
-    m_time_begin = RTT::os::TimeService::Instance()->getTicks();
   }
 
   void Controller::calcPoseDiff(){
-    // position difference expressed in the world frame
+    // Calculate the position difference expressed in the world frame
     KDL::Vector diff_world(m_goal_pose.x - m_current_pose[0], m_goal_pose.y - m_current_pose[1], 0.0);
-    // youbot to world orientation matrix
+    // Calculate the YouBot-to-world orientation matrix, based on the angle
+    // estimation.
     KDL::Rotation rot = KDL::Rotation::RotZ(m_current_pose[2]);
-    // transform to position difference expressed in the Youbot frame
-    KDL::Vector diff_youbot = rot.Inverse(diff_world);
-    m_delta_pose[0] = diff_youbot[0];
-    m_delta_pose[1] = diff_youbot[1];
+    // Express the position difference in the YouBot frame
+    m_delta_pose = rot.Inverse(diff_world);
     m_delta_pose[2] = m_goal_pose.theta - m_current_pose[2];
-    log(Info) << "orientation matrix " << rot << endlog();
-    log(Info) << "delta_pose " << m_delta_pose << endlog();
   }
 
   void Controller::stopHook(){
